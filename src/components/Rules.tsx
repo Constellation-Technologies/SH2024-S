d9203abdf853f90046eb716d08b897ff6e7fdb6c
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-06-16 17:31:51 -0600

    did Rules and Sponsors parts

---
+++ src/components/Rules.tsx
@@ -0,0 +1,25 @@
+import "../styles/Rules.css";
+
+const Rules = () => {
+	return (
+		<div className="rules-container">
+			<img src="/pink-background-wave.png" className="background-wave" alt="" />
+
+			<div className="content">
+				<h2 className="heading">Rules</h2>
+				<p className="text">
+					Lorem ipsum dolor sit amet consectetur. At duis elit dolor egestas.
+					Neque etiam maecenas tincidunt sit varius. Blandit est erat mi auctor
+					lacus ridiculus blandit. Sed sollicitudin ac at risus quam.
+					Scelerisque nec vitae scelerisque odio eleifend urna nisl id. Volutpat
+					enim facilisi a non. Mattis eget ipsum turpis viverra urna urna porta
+					bibendum. Neque facilisis sollicitudin placerat faucibus. Laoreet dui
+					euismod euismod vestibulum dis nam ipsum at. Praesent in cursus nisi
+					nullam.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Rules;
